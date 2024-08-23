dc6724f1a75a145388b91176cff87f06169ca293
lucianaliebl <142036250+lucianaliebl@users.noreply.github.com>
2024-08-23 11:10:46 -0300

    Create script.js

---
+++ Aula 2 - Atividade /script.js
@@ -0,0 +1,19 @@
+var botao = document.getElementById('verificaButton');
+
+//criando evento 
+botao.onclick = function(){
+    var anoNascimento = document.getElementById('idadeInput').value    //captura a idade do formuário 
+    // console .log(idade);  //serve para acompanhar para verificar se está funcionando 
+
+    var idade = 2024 - anoNascimento
+   // document.write(`Sua idade é ${idade}`);
+    
+    var resultado = document.getElementById('idade'); //precisa criar "resultado" no html
+
+    if (idade >= 18){
+        resultado.textContent = `Sua idade é ${idade} anos. Você é maior de idade`;
+    } else {
+        resultado.textContent = `Sua idade é ${idade} anos. Você é menor de idade`; 
+    }
+    
+}
